496c539600efd918a6a28c969ec1f16e82c12bae
Abdim6 <abdi.bilehm6@gmail.com>
2023-02-13 16:48:24 +0100

    ajut un cdt fail

--- src/test/java/testsCase/Addition.java
+++ src/test/java/testsCase/Addition.java
@@ -1,6 +1,8 @@
 
 package testsCase;
 
+import org.junit.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.xpandit.testng.annotations.Xray;
@@ -14,15 +16,18 @@ public class Addition extends Division{
 	public void afficher1() {
 		System.out.println("Ceci est un test avec XRAY 1...");
 	}
-	
+
 	@Test
 	@Xray(test = "MATH-13")
 	public void afficher2() {
 		System.out.println("Ceci est un test avec XRAY 2...");
+		Assert.fail();
 	}
 	@Test
 	@Xray(test = "MATH-14")
 	public void afficher3() {
-		System.out.println("Ceci est un test avec XRAY 2...");
+		System.out.println("Ceci est un test avec XRAY 3...");
 	}
+
+
 }
--- src/test/java/testsCase/Division.java
+++ src/test/java/testsCase/Division.java
@@ -1,6 +1,8 @@
 package testsCase;
 
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -8,16 +10,16 @@ import com.xpandit.testng.annotations.Xray;
 
 public class Division {
 
-//	@BeforeTest
-//	@Xray(requirement = "ABD-11", test = "QSD-08")
-//	public void afficher1() {
-//		System.out.println("Ceci est un test avec XRAY ... Before Test!");
-//	}
-//	
-//	@AfterTest
-//	@Xray(requirement = "ABD-10", test = "QSD-07")
-//	public void afficher2() {
-//		System.out.println("Ceci est un test avec XRAY ... After Test!");
-//	}
+	@BeforeMethod
+	@Xray(requirement = "ABD-11", test = "QSD-08")
+	public void afficher11() {
+		System.out.println("Ceci est un test avec XRAY ... Before Test!");
+	}
+	
+	@AfterTest
+	@Xray(requirement = "ABD-10", test = "QSD-07")
+	public void afficher12() {
+		System.out.println("Ceci est un test avec XRAY ... After Test!");
+	}
 	
 }
