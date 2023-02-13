325a11d692b8c27563e01e02e0852c31cede7dd5
Abdim6 <abdi.bilehm6@gmail.com>
2023-02-13 17:25:20 +0100

    ajout d'un cdt soustraction

--- src/test/java/testsCase/Division.java
+++ src/test/java/testsCase/Division.java
@@ -10,16 +10,16 @@ import com.xpandit.testng.annotations.Xray;
 
 public class Division {
 
-	@BeforeMethod
-	@Xray(requirement = "ABD-11", test = "QSD-08")
-	public void afficher11() {
-		System.out.println("Ceci est un test avec XRAY ... Before Test!");
-	}
-	
-	@AfterTest
-	@Xray(requirement = "ABD-10", test = "QSD-07")
-	public void afficher12() {
-		System.out.println("Ceci est un test avec XRAY ... After Test!");
-	}
+//	@BeforeMethod
+//	@Xray(requirement = "ABD-11", test = "QSD-08")
+//	public void afficher11() {
+//		System.out.println("Ceci est un test avec XRAY ... Before Test!");
+//	}
+//	
+//	@AfterTest
+//	@Xray(requirement = "ABD-10", test = "QSD-07")
+//	public void afficher12() {
+//		System.out.println("Ceci est un test avec XRAY ... After Test!");
+//	}
 	
 }
--- src/test/java/testsCase/Substraction.java
+++ src/test/java/testsCase/Substraction.java
@@ -4,6 +4,11 @@
  */
 package testsCase;
 
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+import com.xpandit.testng.annotations.Xray;
+
 /** 
  * Vu dans Tuto Pavan Kumar  vidéo 39
  * Crée 3 méthodes Test, avec des cas différents passe, failed and skip
@@ -12,5 +17,16 @@ package testsCase;
  * avec parallel
  */
 public class Substraction {
+	@Test
+	@Xray(test = "MATH-10")
+	public void afficher1() {
+		System.out.println("Ceci est un test Soustraction avec XRAY 10 ...");
+	}
 
+	@Test
+	@Xray(test = "MATH-11")
+	public void afficher2() {
+		System.out.println("Ceci est un test Soustraction avec XRAY 11...");
+		
+	}
 }
