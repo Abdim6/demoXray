7443785ac897de1840e8047b58fd7b09953c20fa
Abdim6 <abdi.bilehm6@gmail.com>
2023-02-13 19:26:46 +0100

    ajout plusieurs cdt 7

--- src/test/java/testsCase/Addition1.java
+++ src/test/java/testsCase/Addition1.java
@@ -26,17 +26,17 @@ public class Addition1 extends Division{
 		Assert.assertEquals(true, true);
 	}
 	
-//	@Test
-//	@Xray(test = "MATH-14")
-//	public void afficher3() {
-//		System.out.println("Ceci est un test avec XRAY 3...");
-//		Assert.assertEquals(true, true);
-//	}
-//	@Test
-//	@Xray(test = "MATH-27")
-//	public void afficher4() {
-//		System.out.println("Ceci est un test avec XRAY 3...");
-//		Assert.assertEquals(true, true);
-//	}
+	@Test
+	@Xray(test = "MATH-14")
+	public void afficher3() {
+		System.out.println("Ceci est un test avec XRAY 3...");
+		Assert.assertEquals(true, true);
+	}
+	@Test
+	@Xray(test = "MATH-27")
+	public void afficher4() {
+		System.out.println("Ceci est un test avec XRAY 3...");
+		Assert.assertEquals(true, true);
+	}
 
 }
