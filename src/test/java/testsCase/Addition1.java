a42230c8b1bdce99e1d52c45e5478598f34bdfe6
Abdim6 <abdi.bilehm6@gmail.com>
2023-02-13 19:24:32 +0100

    ajout plusieurs cdt 5

--- src/test/java/testsCase/Addition1.java
+++ src/test/java/testsCase/Addition1.java
@@ -18,24 +18,24 @@ public class Addition1 extends Division{
 //		Assert.assertEquals(false, false);
 		Assert.assertTrue(true);
 	}
-
-	@Test
-	@Xray(test = "MATH-13")
-	public void afficher2() {
-		System.out.println("Ceci est un test avec XRAY 2...");
-		Assert.assertEquals(true, true);
-	}
-	@Test
-	@Xray(test = "MATH-14")
-	public void afficher3() {
-		System.out.println("Ceci est un test avec XRAY 3...");
-		Assert.assertEquals(true, true);
-	}
-	@Test
-	@Xray(test = "MATH-27")
-	public void afficher4() {
-		System.out.println("Ceci est un test avec XRAY 3...");
-		Assert.assertEquals(true, true);
-	}
+//
+//	@Test
+//	@Xray(test = "MATH-13")
+//	public void afficher2() {
+//		System.out.println("Ceci est un test avec XRAY 2...");
+//		Assert.assertEquals(true, true);
+//	}
+//	@Test
+//	@Xray(test = "MATH-14")
+//	public void afficher3() {
+//		System.out.println("Ceci est un test avec XRAY 3...");
+//		Assert.assertEquals(true, true);
+//	}
+//	@Test
+//	@Xray(test = "MATH-27")
+//	public void afficher4() {
+//		System.out.println("Ceci est un test avec XRAY 3...");
+//		Assert.assertEquals(true, true);
+//	}
 
 }
